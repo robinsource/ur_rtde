aff1c37eb3e40a0b98b7e3a85f9025ab5bb72d73
Ken Sørensen <khgs@mmmi.sdu.dk>
2021-09-02 13:08:58 +0200

    Adding move robot using Forcemode

--- test/main.cpp
+++ test/main.cpp
@@ -5,8 +5,10 @@
 #include <ur_rtde/rtde_receive_interface.h>
 #include <vector>
 #include <thread>
+#include <chrono>
 
 using namespace ur_rtde;
+using namespace std::chrono;
 
 // Declare ur_rtde interfaces
 std::shared_ptr<DashboardClient> db_client;
@@ -46,6 +48,9 @@ int main(int argc, char** argv) {
     return res;            // propagate the result of the tests
 
   return res; // the result from doctest is propagated here as well
+
+  // Stop the RTDE control script
+  rtde_control->stopScript();
 }
 
 SCENARIO("move robot in joint space (moveJ)")
@@ -148,6 +153,58 @@ SCENARIO("move robot in tool space using a predefined path")
         for(unsigned int i = 0; i < actual_tcp_pose.size(); i++)
         {
           REQUIRE(actual_tcp_pose[i] == doctest::Approx(target_pose[i]).epsilon(0.005));
+        }
+      }
+    }
+  }
+}
+
+SCENARIO("Move robot in Forcemode (forceMode)")
+{
+  GIVEN("Move robot in cartesian space using fixed force")
+  {
+    // Parameters
+    std::vector<double> task_frame = {0, 0, 0, 0, 0, 0};
+    std::vector<int> selection_vector = {0, 0, 1, 0, 0, 0};
+    std::vector<double> wrench_down = {0, 0, -10, 0, 0, 0};
+    std::vector<double> wrench_up = {0, 0, 10, 0, 0, 0};
+    int force_type = 2;
+    double dt = 1.0/500; // 2ms
+    std::vector<double> limits = {2, 2, 1.5, 1, 1, 1};
+    std::vector<double> joint_q = {-1.54, -1.83, -2.28, -0.59, 1.60, 0.023};
+
+    init_pose = rtde_receive->getActualTCPPose();
+
+    WHEN("robot is still moving")
+    {
+      // Move to initial joint position with a regular moveJ
+      REQUIRE(rtde_control->moveJ(joint_q));
+
+      // Execute 500Hz control loop for a total of 4 seconds, each cycle is ~2ms
+      for (unsigned int i=0; i<2000; i++)
+      {
+        auto t_start = high_resolution_clock::now();
+        // First we move the robot down for 2 seconds, then up for 2 seconds
+        if (i > 1000)
+          rtde_control.forceMode(task_frame, selection_vector, wrench_up, force_type, limits);
+        else
+          rtde_control.forceMode(task_frame, selection_vector, wrench_down, force_type, limits);
+        auto t_stop = high_resolution_clock::now();
+        auto t_duration = std::chrono::duration<double>(t_stop - t_start);
+
+        if (t_duration.count() < dt)
+        {
+          std::this_thread::sleep_for(std::chrono::duration<double>(dt - t_duration.count()));
+        }
+      }
+
+      THEN("robot must be at target")
+      {
+        std::vector<double> actual_tcp_pose = rtde_receive->getActualTCPPose();
+
+        for(unsigned int i = 0; i < actual_tcp_pose.size(); i++)
+        {
+          REQUIRE(actual_tcp_pose[i] == doctest::Approx(init_pose[i]).epsilon(0.005));
         }
       }
     }
