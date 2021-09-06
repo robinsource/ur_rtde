105c99226372b5d952e06b1151bf8d7cb7acdb48
Ken Sørensen <khgs@mmmi.sdu.dk>
2021-09-06 09:30:32 +0200

    Add test of SpeedJ

--- test/main.cpp
+++ test/main.cpp
@@ -281,3 +281,60 @@ SCENARIO("Move robot using servo command (servoJ)")
     }
   }
 }
+
+SCENARIO("Move robot using SpeedJ command (SpeedJ)")
+{
+  GIVEN("Servo to position in joint-space) ")
+  {
+    // Move to initial pose, securing that former test dont leave robot in a strange state.
+    rtde_control->moveL(init_pose, 3, 3);
+
+    // Parameters
+    double acceleration = 0.5;
+    double dt = 1.0/500; // 2ms
+    std::vector<double> joint_q = {-1.54, -1.83, -2.28, -0.59, 1.60, 0.023};
+    std::vector<double> joint_speed = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    // Target is defined in this vector by trial and error
+    std::vector<double> target_pose{0.0897976, -0.390249, 0.1918, -0.768298, 2.76098, -0.74687};
+
+    WHEN("Robot is still moving")
+    {
+      // Move to initial joint position with a regular moveJ
+      REQUIRE(rtde_control->moveJ(joint_q));
+
+      // Execute 500Hz control loop for 2 seconds, each cycle is ~2ms
+      for (unsigned int i=0; i<1000; i++)
+      {
+        auto t_start = high_resolution_clock::now();
+        rtde_control.speedJ(joint_speed, acceleration, dt);
+        joint_speed[0] += 0.0005;
+        joint_speed[1] += 0.0005;
+        auto t_stop = high_resolution_clock::now();
+        auto t_duration = std::chrono::duration<double>(t_stop - t_start);
+
+        if (t_duration.count() < dt)
+        {
+          std::this_thread::sleep_for(std::chrono::duration<double>(dt - t_duration.count()));
+        }
+      }
+
+      THEN("Robot must be at target")
+      {
+        std::vector<double> actual_joint_pose = rtde_receive->getActualTCPPose();
+
+        std::cout << "Size of joint position from robot is " << actual_joint_pose.size() << std::endl;
+        std::cout << "Actual joint positions from robot is ";
+        for (auto i: actual_joint_pose)
+          std::cout << i << ' ';
+        std::cout << std::endl;
+
+        for(unsigned int i = 0; i < actual_joint_pose.size(); i++)
+        {
+          CHECK(actual_joint_pose[i] == doctest::Approx(target_pose[i]).epsilon(0.05));
+
+        }
+      }
+    }
+  }
+}
