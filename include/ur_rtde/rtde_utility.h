cc870b580e401e809951fbd8a91e70178269bb1d
Uwe Kindler <uwe_kindler@web.de>
2023-06-06 14:16:12 +0200

    Fix "NOMINMAX" redefined warnings

--- include/ur_rtde/rtde_utility.h
+++ include/ur_rtde/rtde_utility.h
@@ -22,7 +22,9 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #define WIN32_LEAN_AND_MEAN
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #else
 #include <pthread.h>
